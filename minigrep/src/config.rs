e70fa90a7ab04e6bb1095c3fd526475abdc4b535
Rob Bertram <rbertram8@gmail.com>
2023-11-06 21:53:33 -0500

    remove unnecessary cloning

--- minigrep/src/config.rs
+++ minigrep/src/config.rs
@@ -1,21 +1,21 @@
 use std::env;
 
 #[derive(Debug, Clone)]
-pub struct Config {
-    pub query: String,
-    pub path: String,
+pub struct Config<'a> {
+    pub query: &'a str,
+    pub path: &'a str,
     pub ignore_case: bool,
 }
 
-impl Config {
-    pub fn new(args: &[String]) -> Result<Self, String> {
-        let query = args.get(1).ok_or("Missing query".to_string())?;
-        let path = args.get(2).ok_or("Missing path".to_string())?;
+impl<'a> Config<'a> {
+    pub fn new(args: &'a [String]) -> Result<Self, &str> {
+        let query = args.get(1).ok_or("Missing query")?;
+        let path = args.get(2).ok_or("Missing path")?;
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Self {
-            query: query.clone(),
-            path: path.clone(),
+            query,
+            path,
             ignore_case,
         })
     }
